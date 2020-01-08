519bb34b887cfc2666fc3d834e3295c4ffab3227
Glitch (libproauth0) <none>
2020-01-08 02:07:38 +0000

    Updated Site URL to env var

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const strategy = new Auth0Strategy(
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: 'https://suffolkcountylibraryprograms.herokuapp.com/callback'
+    callbackURL: process.env.SITE_URL + 'callback'
   },
   function(accessToken, refreshToken, extraParem, profile, done){
     return done(null, profile);
@@ -219,7 +219,7 @@ app.get('/deleteProgram/:id', function(req, res){
 app.get('/login', passport.authenticate('auth0', {
     clientID: process.env.AUTH0_CLIENT_ID, 
     domain: process.env.AUTH0_DOMAIN, 
-    redirectUri: 'https://suffolkcountylibraryprograms.herokuapp.com/',
+    redirectUri: process.env.SITE_URL,
     responseType: 'code',
     audience: 'https://dev-5uhhmfa3.auth0.com/api/v2/',
     scope: 'openid email profile'}),
@@ -241,7 +241,7 @@ app.get('/logout', function (req,res) {
   //auth0 logout
    const logoutUrl = 'https://' + process.env.AUTH0_DOMAIN + 
                     '/v2/logout?client_id=' + process.env.AUTH0_CLIENT_ID + 
-                    '&returnTo=https://suffolkcountylibraryprograms.herokuapp.com/';        
+                    '&returnTo=' + process.env.SITE_URL;        
    res.redirect(logoutUrl);
     })
   }
@@ -256,7 +256,7 @@ app.get('/callback', function (req, res, next) {
       return next(err); 
     }
     if (!user) { 
-      return res.redirect('/failure'); }
+      return res.redirect('/verify'); }
     req.logIn(user, function (err) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
@@ -267,8 +267,8 @@ app.get('/callback', function (req, res, next) {
 });
 
 
-app.get('/failure', function(req,res,next) {
-  res.render('failure');
+app.get('/verify', function(req,res,next) {
+  res.render('verify');
 });
 
 app.get('/nonLibEmail', function(req,res,next) {
